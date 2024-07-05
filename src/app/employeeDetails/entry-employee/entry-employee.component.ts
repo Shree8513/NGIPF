121bbb17893f1d6e2a4ad1b82fb99e3a01ab3de9
baishakhi <baishakhi212001@gmail.com>
2024-07-05 17:11:24 +0530

    employee entry personalDetails done

--- src/app/employeeDetails/entry-employee/entry-employee.component.ts
+++ src/app/employeeDetails/entry-employee/entry-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {MenuItem, MessageService} from 'primeng/api';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-entry-employee',
@@ -7,42 +8,35 @@ import {MenuItem, MessageService} from 'primeng/api';
   styleUrls: ['./entry-employee.component.scss']
 })
 export class EntryEmployeeComponent implements OnInit {
-
-    items: MenuItem[];
-
-    activeIndex:number=0;
-
-    constructor(public messageService: MessageService) {}
+    activeState: boolean[] = [true,false,false];
+    toggle(index: number) {
+        this.activeState[index] = !this.activeState[index];
+    }
+    addPersonal!: FormGroup;
+    maxDateValue: Date = new Date(Date.now());
+    constructor(private fb: FormBuilder) {}
 
     ngOnInit() {
-        this.items = [{
-                label: 'Basic',
-                 command: (event: any) => {
-                    this.activeIndex = 0;
-                    this.messageService.add({severity:'info', summary:'First Step', detail: event.item.label});
+        this.initializeForm()
+    }
+    initializeForm(): void {
+        this.addPersonal = this.fb.group({
+            empID: ['', Validators.required],
+            pfAcc:['', Validators.required],
+            name:['', Validators.required],
+            gender: ['', Validators.required],
+            religion: ['', Validators.required],
+            selectedDOB: [null, Validators.required],
+            pan: ['', Validators.required],
+            adhaar:['', Validators.required],
+            maritalStatus: ['', Validators.required],
+            selectedEffectDate:[null, Validators.required],
+        });
+    }
 
-             },
-                routerLink: 'basic'
-            },
-            {
-                label: 'Contact',
-                routerLink: 'contact'
-            },
-            {
-                label: 'Salary',
-                routerLink: 'salary'
-            },
-            {
-                label: 'Office',
-                routerLink: 'office'
-            },
-            {
-                label: 'Bank',
-                routerLink: 'bank'
-            }
-        ];
-
-        
+    isInvalidAndTouched(controlName: string): boolean {
+        const control = this.addPersonal.get(controlName);
+        return control && control.invalid && (control.dirty || control.touched);
     }
 
 }
