c34df74b4357af003773e8cb0b4662723bccf8d9
Ashokkumar1302 <ashokvishwakarma1310@gmail.com>
2024-07-11 14:41:47 +0530

    employee tr code UI update

--- src/app/core/models/employeeDetails/employeeDetails.ts
+++
@@ -1,9 +0,0 @@
-export interface searchEmployee {
-    Treasury: string;
-    SanctionAdmin: string;
-    SalarySource: any;
-    PFDAdmin: string;
-    RecommendingAuthority: string;
-    EmployeeId: number;
-    PFAccountNumber: string;
-}
--- src/app/core/services/employeeDetails/employee-details.service.ts
+++ src/app/core/services/employeeDetails/employee-details.service.ts
@@ -3,19 +3,20 @@ import { Injectable } from '@angular/core';
 import { ToastService } from '../toast.service';
 import { IapiResponce } from '../../models/iapi-responce';
 import { Observable, catchError } from 'rxjs';
-import { searchEmployee } from '../../models/employeeDetails/employeeDetails';
 import { environment } from 'src/environments/environment';
+import { error } from 'console';
+import { Message } from 'primeng/api';
 @Injectable({
     providedIn: 'root',
 })
 export class EmployeeDetailsService {
     BaseURL: string = `${environment.BaseURL}`;
-    constructor(private http: HttpClient, private toastService: ToastService) {}
+    constructor(private http: HttpClient, private toastService: ToastService) { }
 
-    searchEmployee(payload: searchEmployee): Observable<IapiResponce> {
-        return this.http.post<IapiResponce>(this.BaseURL + 'api/EmployeeDetails/SearchEmployee', payload).pipe(
+    getTresury(): Observable<IapiResponce> {
+        return this.http.get<IapiResponce>(this.BaseURL + 'api/Treasery/GetAllTreasury').pipe(
             catchError((error) => {
-                throw this.toastService.showError(error.message);
+                throw this.toastService.showError(error.Message);
             })
         );
     }
